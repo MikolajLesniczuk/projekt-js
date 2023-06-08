b4d5dd6228be2356691e4ac3e20f95e01ff5af3c
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2023-06-08 10:47:51 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import './sass/main.scss';
 
 
 async function fetchFilms(page = 1) {
+    try{
     const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=6fc014c055bacb8460b83603c43b9093&page=${page}&per_page=9`);
     const data = await response.json();
     console.log(data.results);
     renderGallery(data.results);
     renderPagination(data.total_pages);
     return data;
+    }
+    catch(error) {
+        console.log(error.toString())
+    }
   }
   
   const gallery = document.querySelector('.gallery');
